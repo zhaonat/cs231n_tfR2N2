4279f4e6c6884326080cbcd5eaba421206d69863
Yi-61 <33105174+Yi-61@users.noreply.github.com>
2018-05-30 01:56:33 -0700

    Add files via upload

--- modules/decoder.py
+++ modules/decoder.py
@@ -38,19 +38,19 @@ def decoder_res_conv3dTranspose( inputs, n_deconvfilter):
 
     # Define the model structure
     # X = tf.keras.layers.UpSampling3D( size = [2, 2, 2] )(inputs) #0.71, 0.70
-    unpool1 = tf.layers.conv3d_transpose( inputs=inputs, filters = 2 , kernel_size=(2,2,2), strides = 2, padding="same") #0.75
+    unpool1 = tf.layers.conv3d_transpose( inputs=inputs, filters = n_deconvfilter[1] , kernel_size=(2,2,2), strides = 2, padding="same") #0.75
     X = tf.layers.conv3d( inputs = unpool1, filters = n_deconvfilter[1], kernel_size = 3, padding="same", activation = tf.nn.leaky_relu)
     X = tf.layers.conv3d( inputs = X, filters = n_deconvfilter[1], kernel_size = 3, padding="same", activation = tf.nn.leaky_relu)
     X = X + unpool1
 
     # X = tf.keras.layers.UpSampling3D( size = [2, 2, 2] )(X)
-    unpool2 = tf.layers.conv3d_transpose( inputs=X, filters = 2 , kernel_size=(2,2,2), strides = 2, padding="same")
+    unpool2 = tf.layers.conv3d_transpose( inputs=X, filters = n_deconvfilter[2] , kernel_size=(2,2,2), strides = 2, padding="same")
     X = tf.layers.conv3d( inputs = unpool2, filters = n_deconvfilter[2], kernel_size = 3, padding="same", activation = tf.nn.leaky_relu )
     X = tf.layers.conv3d( inputs = X, filters = n_deconvfilter[2], kernel_size = 3, padding="same", activation = tf.nn.leaky_relu )
     X = X + unpool2
 
     # X = tf.keras.layers.UpSampling3D( size = [2, 2, 2] )(X)
-    unpool3 = tf.layers.conv3d_transpose( inputs=X, filters = 2 , kernel_size=(2,2,2), strides = 2, padding="same")
+    unpool3 = tf.layers.conv3d_transpose( inputs=X, filters=n_deconvfilter[3] , kernel_size=(2,2,2), strides = 2, padding="same")
     X = tf.layers.conv3d( inputs = unpool3, filters = n_deconvfilter[3], kernel_size = 3, padding="same", activation = tf.nn.leaky_relu )
     X = tf.layers.conv3d( inputs = X, filters = n_deconvfilter[3], kernel_size = 3, padding="same", activation = tf.nn.leaky_relu )
     conv3 = tf.layers.conv3d( inputs = unpool3, filters = n_deconvfilter[3], kernel_size = 1, padding="same", activation = tf.nn.leaky_relu )
@@ -75,19 +75,19 @@ def decoder_res_upsample( inputs, n_deconvfilter):
     # initializer = tf.variance_scaling_initializer(scale=2.0)
 
     # Define the model structure
-    unpool1 = tf.keras.layers.UpSampling3D( size = [2, 2, 2] )(inputs) #0.71, 0.70
+    unpool1 = tf.keras.layers.UpSampling3D( size = [2, 2, 2], filters = n_deconvfilter[1] )(inputs) #0.71, 0.70
     # unpool1 = tf.layers.conv3d_transpose( inputs=inputs, filters = 2 , kernel_size=(2,2,2), strides = 2, padding="same") #0.75
     X = tf.layers.conv3d( inputs = unpool1, filters = n_deconvfilter[1], kernel_size = 3, padding="same", activation = tf.nn.leaky_relu)
     X = tf.layers.conv3d( inputs = X, filters = n_deconvfilter[1], kernel_size = 3, padding="same", activation = tf.nn.leaky_relu)
     X = X + unpool1
 
-    unpool2 = tf.keras.layers.UpSampling3D( size = [2, 2, 2] )(X)
+    unpool2 = tf.keras.layers.UpSampling3D( size = [2, 2, 2], filters=n_deconvfilter[2] )(X)
     # unpool2 = tf.layers.conv3d_transpose( inputs=X, filters = 2 , kernel_size=(2,2,2), strides = 2, padding="same")
     X = tf.layers.conv3d( inputs = unpool2, filters = n_deconvfilter[2], kernel_size = 3, padding="same", activation = tf.nn.leaky_relu )
     X = tf.layers.conv3d( inputs = X, filters = n_deconvfilter[2], kernel_size = 3, padding="same", activation = tf.nn.leaky_relu )
     X = X + unpool2
 
-    unpool3 = tf.keras.layers.UpSampling3D( size = [2, 2, 2] )(X)
+    unpool3 = tf.keras.layers.UpSampling3D( size = [2, 2, 2], filters=n_deconvfilter[3] )(X)
     # unpool3 = tf.layers.conv3d_transpose( inputs=X, filters = 2 , kernel_size=(2,2,2), strides = 2, padding="same")
     X = tf.layers.conv3d( inputs = unpool3, filters = n_deconvfilter[3], kernel_size = 3, padding="same", activation = tf.nn.leaky_relu )
     X = tf.layers.conv3d( inputs = X, filters = n_deconvfilter[3], kernel_size = 3, padding="same", activation = tf.nn.leaky_relu )
