41e3a0092cc9dba80635a2fd917e3f0f0d5d1d05
Nathan Zhao <zhaonat@gmail.com>
2018-06-04 20:59:31 -0700

    new batch train file

--- test_scripts/R2N2_large_batch_training.py
+++ test_scripts/R2N2_large_batch_training.py
@@ -4,6 +4,10 @@ from modules import encoder
 from modules import decoder
 from modules import simple_lstm
 from modules import my_3d_lstm
+from modules import my_3d_gru
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.model_selection import train_test_split
 import settings
 import os
 import pickle
@@ -14,52 +18,67 @@ from data_processing import pickle_to_data
 this takes a while to run
 '''
 from data_processing import subsample_voxel
-data_dir = os.path.join(settings.ROOT_DIR, 'data', 'shape_net_training_test');
-f = open(os.path.join(data_dir, 'R2N2_1024_batch_1.p'), 'rb');
+data_dir = os.path.join(settings.ROOT_DIR, 'data');
+f = open(os.path.join(data_dir, 'planes_4045_batch', \
+                      'planes_cropped_grayscale','planes_02691156_4045_batch_cropped_grayscale.p'), 'rb');
 #f = open(os.path.join(data_dir, 'R2N2_128_batch_1.p'), 'rb');
+class_name = 'planes';
+X,y  = pickle.load(f);
 
-batch_sample = pickle.load(f);
-print(batch_sample.keys())
-sample_keys = list(batch_sample.keys())
-X, y = pickle_to_data.unravel_batch_pickle(batch_sample)
+X_train,X_test, y_train, y_test = train_test_split(X,y,test_size = 0.4);
+## train test split
+print(X_train.shape)
+print(X_test.shape)
+
 y0 = y;
-X_nparr = np.array(X);
+X_nparr = X_train;
 batch_size,T,H,W,C = X_nparr.shape;
 
-
-## perform downsampling
-down_y = list();
-strides = [1,1,1]
-for i in range(len(y)):
-    down_y.append(subsample_voxel.downsample(y[i], strides))
-sample_model = down_y[0]; print(sample_model.shape)
-[NX,NY,NZ] = sample_model.shape;
-
-## convert everything to arrays
-y = np.array(down_y)
-print('voxel shape')
 print(y.shape)
-#convert to one hot
-y_one_hot = np.stack((y==0, y==1))
-y_one_hot = np.transpose(y_one_hot, axes = [1,2,3,4,0])
-# plt.figure();
-# plt.imshow(sample_image[:,:,0:3]);
-# plt.show()
+batch_size,NX,NY,NZ = y.shape
 
-mini_batch_size = 8;
+## if you want, take a smaller subset to test
+subset = 256;
+X_train = X_train[0:subset,:,:,:,:];
+y_train = y_train[0:subset];
 
-#sequence_placeholder = tf.placeholder(tf.float32, shape = [batch_size,T,H,W,C])
-sequence_placeholder = tf.placeholder(tf.float32, shape = [T,mini_batch_size ,H,W,C])
-image_placeholder = tf.placeholder(tf.float32, shape = [mini_batch_size ,H,W,C])
+batch_size,T,H,W,C = X_train.shape;
+
+epochs = 500;
+mini_batch_size = 32;
+
 #need the 2 because of one-hot encoding for softmax
-label_placeholder = tf.placeholder(tf.float32, shape = [mini_batch_size , NX,NY,NZ, 2]);
+label_placeholder = tf.placeholder(tf.float32, shape = [mini_batch_size , NX,NY,NZ], name = 'voxel');
 
-## specify total graph
-sequence = [image_placeholder for i in range(T)]
+## =========================== TUNE-ABLE PARAMETERS ================================================##
+sub_sequence_length = 2;
+conv_lstm_hidden_channels = 24
+
+sequence = []
+for i in range(sub_sequence_length):
+    sequence.append(tf.placeholder(tf.float32, shape = [mini_batch_size ,H,W,C], name = 'img_'+str(i)))
+
 encoded_sequence = list();
+print('sequence length: '+str(len(sequence)))
 for image in sequence:
-    encoded_out = encoder.encoder(image);
+    #encoded_out = encoder.encoder_resnet2(image,start_filter_array = [4,4,4], end_filter_array = [8,8,4]);
+    encoded_out = encoder.encoder_resnet1(image);
+
     encoded_sequence.append(encoded_out);
+
+total_parameters = 0
+for variable in tf.trainable_variables():
+    # shape is an array of tf.Dimension
+    shape = variable.get_shape()
+    print(shape)
+    print(len(shape))
+    variable_parameters = 1
+    for dim in shape:
+        print(dim)
+        variable_parameters *= dim.value
+    print(variable_parameters)
+    total_parameters += variable_parameters
+print('total: '+str(total_parameters))
 
 #check type of encoded sequence
 print(type(encoded_sequence));
@@ -73,7 +92,7 @@ print(encoded_sequence.shape)
 
 ## after we use the encoder, we should have a sequence of dense outputs
 #conv_lstm_hidden = simple_lstm.simple_lstm(encoded_sequence);
-conv_lstm_hidden = my_3d_lstm.my_3d_lstm(encoded_sequence, cube_size=4, num_channels=4, filter_size=3)
+conv_lstm_hidden = my_3d_gru.my_3d_gru(encoded_sequence, cube_size=4, num_channels=conv_lstm_hidden_channels, filter_size=3)
 
 
 print('lstm hidden shape:')
@@ -81,32 +100,40 @@ print(conv_lstm_hidden.shape)
 
 ##decode the lstm output, which is just the hidden state, 3D
 # pass it through the decoder
-n_deconvfilter = [128, 128, 128, 64, 32, 2]
+n_deconvfilter = [32,32,16,16,8, 1]
 logits = decoder.decoder_res_conv3dTranspose(conv_lstm_hidden, n_deconvfilter)
 
 #squeeze logits so it is 4 dimensional
 logits = tf.squeeze(logits);
+outputs = tf.nn.sigmoid(logits);
+
+outputs = tf.identity(outputs, name='classification_activation');
+
 #check shape
 print('final output shape:')
 print(logits.shape)
 
-outputs = tf.contrib.layers.softmax(logits)
-print(outputs.shape)
 print(label_placeholder.shape)
-loss = tf.nn.softmax_cross_entropy_with_logits_v2(
-    labels=label_placeholder,
-    logits=logits,
+# loss = tf.nn.sigmoid_cross_entropy_with_logits(
+#     labels=label_placeholder,
+#     logits=logits,
+# )
+
+loss=tf.nn.weighted_cross_entropy_with_logits(
+    targets = label_placeholder,
+    logits = logits,
+    pos_weight = 10,
+    name=None
 )
+
 loss = tf.layers.flatten(loss)
 loss = tf.reduce_mean(loss);
 
 ## define optimizer
-optimizer = tf.train.AdamOptimizer(5e-4)
+optimizer = tf.train.AdamOptimizer(5e-5)
 optimizer = optimizer.minimize(loss)
 
-#
-predictions = tf.argmax(outputs, axis = 4)
-print(predictions.shape)
+
 
 ## =================== RUN TENSORFLOW SESSION ==================##
 #Create a saver object which will save all the variables
@@ -115,63 +142,68 @@ saver = tf.train.Saver()
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
-epochs = 400;
-loss_history = list(); accuracy = list()
-
+loss_history = list(); training_accuracy = list()
 num_batches = int(batch_size/mini_batch_size);
+training_iou = [];
 for epoch in range(epochs):
 
     for i in range(num_batches):
         #need to determine X_final
         start = i*mini_batch_size; end = (i+1)*mini_batch_size;
-        print(sample_keys[start:end])
-        y_batch = y_one_hot[start:end];
-        y_batch_flat = y[start:end]
+        y_batch = y[start:end]
         # print('y_batch shape')
-        # print(y_batch.shape)
-        X_batch = X_nparr[start:end, :, :, :, :]
+
+        X_batch = X_train[start:end, :, :, :, :]
         X_batch = np.transpose(X_batch, axes=[1, 0, 2, 3, 4])
         ## convert this back to a list of arrs
-        X_final = [X_batch[t, :, :, :, :] for t in range(T)];
+
+        #pick a random sample of perspectives: DON'T DO THAT
+        #t_sample = np.random.randint(0, T, sub_sequence_length)
+        t_sample = np.arange(0, sub_sequence_length);
+
+        X_final = [X_batch[t, :, :, :, :] for t in t_sample];
 
         feed_dict_input = {i: d for i, d in zip(sequence, X_final)};
         feed_dict_input[label_placeholder] = y_batch;
+        #print(feed_dict_input.keys())
         #manually insert the label_placeholder
+        loss_epoch, prediction, _ = sess.run([loss, outputs, optimizer], feed_dict=feed_dict_input)
 
+        prediction = prediction > 0.5;
+        training_accuracy.append(np.mean(prediction == y_batch))
+    epoch_training_iou = [];
+    for j in range(mini_batch_size):
+        epoch_training_iou.append(iou.IoU_3D(y_batch[j, :, :, :], prediction[j, :, :, :]))
+    training_iou.append(np.mean(epoch_training_iou))
+    training_iou.append(np.mean(epoch_training_iou))
+    print(np.mean(training_iou))
 
-        sess.run(optimizer, feed_dict = feed_dict_input)
-        loss_epoch = sess.run(loss, feed_dict = feed_dict_input)
-
-        #reconfigure feed_dict to accept a place_holder for y
-        prediction = sess.run(predictions, feed_dict = feed_dict_input)
-
-        accuracy.append(np.mean(prediction == y_batch_flat))
-    for k in range(mini_batch_size):
-        print(iou.IoU_3D(y_batch_flat[k, :, :, :], prediction[k, :, :, :]))
     print('epoch: '+str(epoch)+' loss: '+str(loss_epoch))
-    print(prediction.shape)
-    print(np.mean(prediction == y_batch_flat))
+    prediction = np.squeeze(prediction)
+    print(np.mean(prediction == y_batch))
     loss_history.append(loss_epoch);
+    if(epoch%20 == 0 and epoch > 1): #save it occassionally in case we stop the run
+        pickle.dump([loss_history, training_iou, training_accuracy], open('gray_scale_planes_training_data.p', 'wb'));
+        saver.save(sess, './grayscale_plane_R2N2_model_weights', global_step=epoch)
+        #we can artificially rename the checkpoint? probably not
+
         #predictions = tf.argmax(outputs, axis = 4)
 
-saver.save(sess, './R2N2_model_weights', global_step = epochs)
+save_path = saver.save(sess, "./"+class_name+".ckpt")
+saver.save(sess, './grayscale_plane_R2N2_model_weights', global_step = epochs)
 
 
 ## run test batch
-f = open(os.path.join(data_dir, 'R2N2_128_batch_1.p'), 'rb');
-batch_sample_2 = pickle.load(f);
-X, y = pickle_to_data.unravel_batch_pickle(batch_sample_2)
-X_nparr = np.array(X);
-batch_size,T,H,W,C = X_nparr.shape;
-num_batches = int(128/mini_batch_size);
 test_iou = list();
 print('run test case')
+test_samples, T,W,H,C = X_test.shape;
+num_test_batches = int(test_samples/mini_batch_size);
 
-for i in range(num_batches-1):
+for i in range(num_test_batches):
     start = i * mini_batch_size;
     end = (i + 1) * mini_batch_size;
 
-    X_batch = X_nparr[start:end, :, :, :, :]
+    X_batch = X_test[start:end, :, :, :, :]
 
     X_batch = np.transpose(X_batch, axes=[1, 0, 2, 3, 4])
 
@@ -180,55 +212,22 @@ for i in range(num_batches-1):
 
 
     ## perform downsampling
-    down_y = list();
-    strides = [1,1,1]
-    for i in range(len(y)):
-        down_y.append(subsample_voxel.downsample(y[i], strides))
-    sample_model = down_y[0]; print(sample_model.shape)
-    [NX,NY,NZ] = sample_model.shape;
-    ## convert everything to arrays
-    y = np.array(down_y)
-    y_one_hot = np.stack((y==0, y==1))
-    y_one_hot = np.transpose(y_one_hot, axes = [1,2,3,4,0])
     feed_dict_input = {i: d for i, d in zip(sequence, X_final)}; #size has to be fixed...
-    feed_dict_input[label_placeholder] = y_one_hot[start:end];
+    feed_dict_input[label_placeholder] = y_test[start:end];
     test_loss= sess.run(loss, feed_dict=feed_dict_input)
-    test_prediction = sess.run(predictions, feed_dict=feed_dict_input)
+    test_prediction = sess.run(outputs, feed_dict=feed_dict_input)
+    test_prediction = test_prediction > 0.5;
     for k in range(mini_batch_size):
         print(iou.IoU_3D(y[k, :, :, :], test_prediction[k, :, :, :]))
         test_iou.append(iou.IoU_3D(y[k, :, :, :], test_prediction[k, :, :, :]))
 
 print('test_loss')
 print(test_loss)
+print('number of test ex: '+str(len(test_iou)))
 print(np.mean(test_iou))
 
-#Now, save the graph
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
-for j in range(batch_size-1):
-    fig = plt.figure(figsize = (20,8))
-    ax = fig.add_subplot(1,2, 1, projection='3d')
-    ax.voxels(prediction[j,:,:,:], edgecolor='k')
-    ax.set(xlabel='x', ylabel='y', zlabel='z', title='prediction')
-
-    ax = fig.add_subplot(1,2, 2, projection='3d')
-    ax.voxels(y[j,:,:,:], edgecolor='k', facecolor = 'blue')
-    ax.set(xlabel='x', ylabel='y', zlabel='z', title='original model')
-
-plt.show();
+pickle.dump([loss_history, training_iou, training_accuracy, test_iou], open('gray_scale_planes_training_data.p', 'wb'));
 
 
-plt.figure();
-plt.subplot(121)
-plt.plot(loss_history)
-plt.xlabel('epoch')
-plt.ylabel('loss')
-plt.title('Sample R2N2 Preliminary Module Loss')
-plt.subplot(122)
-plt.plot(accuracy)
-plt.xlabel('epoch')
-plt.ylabel('accuracy')
-plt.title('Sample R2N2 Preliminary Module Acc')
-plt.tight_layout();
-plt.show()
+
+
