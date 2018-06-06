af0e1d67c8013b84144543c2e8895e3e055a3ee6
Nathan Zhao <zhaonat@gmail.com>
2018-06-06 01:24:56 -0700

    simple encoder sequence

--- modules/encoder.py
+++ modules/encoder.py
@@ -33,6 +33,43 @@ def encoder(input_image):
     #run this
     return dense;
 
+
+def encoder_sequence(input_batch_list):
+    '''
+    #input image should be a tensorflow tensor
+    should an input 127x127 rgb image (W x H x C); #color channels should be last dim
+    should ouput a dense representation or encoding
+    '''
+    encoded_list = list();
+    for input_image in input_batch_list:
+
+        conv7 = tf.layers.conv2d(
+            inputs=input_image,
+            filters=32,
+            kernel_size=[1, 1],
+            padding="same",
+            activation=tf.nn.relu)
+        pool7 = tf.layers.max_pooling2d(inputs=conv7, pool_size=[2, 2], strides=2)
+
+        # use a for loop for the remaining 5 3x3 convs
+        for i in range(5):
+            conv3 = tf.layers.conv2d(
+                inputs=pool7,
+                filters=32,
+                kernel_size=[3, 3],
+                padding="same",
+                activation=tf.nn.relu)
+            pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2], strides=2)
+            pool7 = pool3;
+
+        # add in dense layer
+        pool_flat = tf.contrib.layers.flatten(pool7)
+        dense = tf.layers.dense(inputs=pool_flat, units=1024, activation=tf.nn.relu)
+        encoded_list.append(dense);
+    # run this
+    return encoded_list;
+
+
 def encoder_z(input_batch):
     conv7 = tf.layers.conv2d(inputs=input_batch, filters=32, kernel_size=(2, 2))
     # use a for loop for the remaining 5 3x3 convs
